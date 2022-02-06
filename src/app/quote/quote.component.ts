3b394602f00161af8adf53f0fef2200de8cbd383
Thuku7 <robethuku11@gmail.com>
2022-02-06 22:36:47 +0300

    Will add the place holder text for the quote app and nest the quote component in the main html file

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+
+  quotes : Quote[] = [
+    new Quote(1,'Kung-lao',' “Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habits. Watch your habits; they become character. Watch your character; it becomes your destiny.”','Lao-Tze',new Date(2019,7,14)),
+    new Quote(2,'Diaz','“Live for each second without hesitation.” ','Elton John',new Date()),
+    new Quote(3,'ArchySlap','. “Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” ','Steve Jobs',new Date(2020,2,1)),
+    new Quote(4,'OneTwo',' “Life is like riding a bicycle. To keep your balance, you must keep moving.” ','Albert Einstein',new Date(2018,6,21)),
+    new Quote(5,'Turkish','“Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.”','Kevin Hart',new Date(2020,2,22)),
+  ]
 
   constructor() { }
 
